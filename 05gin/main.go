7969d22b81a126e76acadccce0822db025d366a6
chensq <chensiqi_scdy@163.com>
2020-03-08 16:49:20 +0800

    uri数据的解析与绑定

--- 05gin/main.go
+++ 05gin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,7 +11,7 @@ import (
 type Login struct {
 	// binding:"required"修饰的字段是必需字段，若接收为空，报错
 	User     string `form:"user" json:"user" uri:"user" xml:"user" binding:"required"`
-	Password string `form:"password" json:"password"`
+	Password string `form:"password" json:"password" uri:"password" xml:"password" binding:"required"`
 }
 
 // json数据的解析和绑定
@@ -31,7 +32,7 @@ func loginJson(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"msg": "注册成功"})
+	ctx.JSON(http.StatusOK, gin.H{"code": "200", "msg": "注册成功"})
 }
 
 // 表单数据解析和绑定
@@ -44,18 +45,40 @@ func loginForm(ctx *gin.Context) {
 		return
 	}
 
-	// 判断用户名密码是否正确
 	if !(formData.User == "cdd" && formData.Password == "cdd123") {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "用户名或密码错误"})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"msg": "注册成功"})
+	ctx.JSON(http.StatusOK, gin.H{"code": "200", "msg": "注册成功"})
+}
+
+// URI数据的解析与绑定
+func loginURI(ctx *gin.Context) {
+	var uriData Login
+
+	if err := ctx.ShouldBindUri(&uriData); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	fmt.Println(uriData.User, uriData.Password)
+	fmt.Println(ctx.Param("password"))
+
+	// ctx.Param("user")
+	// ctx.Param("password")
+
+	if !(uriData.User == "cdd" && uriData.Password == "cdd123") {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "用户名或密码错误"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"code": "200", "msg": "注册成功"})
 }
 
 func main() {
 	r := gin.Default()
 	r.POST("/loginjson", loginJson)
 	r.POST("/loginform", loginForm)
+	r.GET("/:user/:password", loginURI)
 	r.Run(":9000")
 }
