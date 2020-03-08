08282a3e2d9ccf771825cf23defc3194e8e084d2
chensq <chensiqi_scdy@163.com>
2020-03-08 16:18:24 +0800

    表单数据的解析与绑定

--- 05gin/go.mod
+++ 05gin/go.mod
@@ -1,3 +1,5 @@
 module gin-study/05gin
 
 go 1.14
+
+require github.com/gin-gonic/gin v1.5.0 // indirect
--- 05gin/main.go
+++ 05gin/main.go
@@ -34,8 +34,28 @@ func loginJson(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"msg": "注册成功"})
 }
 
+// 表单数据解析和绑定
+func loginForm(ctx *gin.Context) {
+	var formData Login
+	// Bind()默认解析并绑定form格式
+	// 根据请求头中content-type自动推断
+	if err := ctx.Bind(&formData); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// 判断用户名密码是否正确
+	if !(formData.User == "cdd" && formData.Password == "cdd123") {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "用户名或密码错误"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"msg": "注册成功"})
+}
+
 func main() {
 	r := gin.Default()
 	r.POST("/loginjson", loginJson)
+	r.POST("/loginform", loginForm)
 	r.Run(":9000")
 }
