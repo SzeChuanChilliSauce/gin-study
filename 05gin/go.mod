36e96e323cce762619089d0aa977817c164922d9
chensq <chensiqi_scdy@163.com>
2020-03-13 17:28:57 +0800

    返回多种数据格式

--- 05gin/go.mod
+++ 05gin/go.mod
@@ -2,4 +2,7 @@ module gin-study/05gin
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.5.0 // indirect
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/golang/protobuf v1.3.5
+)
--- 05gin/main.go
+++ 05gin/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin/testdata/protoexample"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -77,8 +78,50 @@ func loginURI(ctx *gin.Context) {
 
 func main() {
 	r := gin.Default()
-	r.POST("/loginjson", loginJson)
-	r.POST("/loginform", loginForm)
+	r.POST("/login_json", loginJson)
+	r.POST("/login_form", loginForm)
 	r.GET("/:user/:password", loginURI)
+
+	// 多种响应方式
+	// 1.json
+	r.GET("/resp_json", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"message": "somejson", "status": 200})
+	})
+
+	// 2.结构体
+	r.GET("/resp_struct", func(ctx *gin.Context) {
+		var msg struct {
+			Name    string
+			Message string
+			Number  int
+		}
+
+		msg.Message = "hello"
+		msg.Name = "world"
+		msg.Number = 10086
+		ctx.JSON(200, msg)
+	})
+
+	// 3.XML
+	r.GET("/resp_xml", func(ctx *gin.Context) {
+		ctx.XML(200, gin.H{"message": "abcd"})
+	})
+
+	// 4.YAML
+	r.GET("/resp_yaml", func(ctx *gin.Context) {
+		ctx.YAML(200, gin.H{"name": "cdd"})
+	})
+
+	// 5 protobuf
+	r.GET("/resp_protobuf", func(ctx *gin.Context) {
+		reps := []int64{0, 1, 2}
+		label := "label"
+		data := &protoexample.Test{
+			Label: &label,
+			Reps:  reps,
+		}
+		ctx.ProtoBuf(200, data)
+	})
+
 	r.Run(":9000")
 }
